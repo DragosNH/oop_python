402097712547c151ff1869920e76d565c81f7246
Dragoş <edragos33@gmail.com>
2025-09-03 16:16:03 +0200

    added character class

---
+++ Character.py
@@ -0,0 +1,9 @@
+class Character:
+    def __init__(self, name, race, char_class, weapon, health_points, mana_points):
+        self.name = name
+        self.race = race
+        self.char_class = char_class
+        self.weapon = weapon
+        self.health_points = health_points
+        self.mana_points = mana_points
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import Character
+
 def main():
-    print("Hello world!")
+    aragorn = Character.Character("Aragorn", "Human", "Ranger", 100, 100)
+    print(aragorn)
 
 if __name__ == "__main__":
     main()
