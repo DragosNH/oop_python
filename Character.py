627f79465b391432f48ec4ec60d34e29ff5dc31a
Dragoş <edragos33@gmail.com>
2025-09-03 18:54:18 +0200

    race bonus added

--- Character.py
+++ Character.py
@@ -1,11 +1,13 @@
+from races import Race
+
 class Character:
     def __init__(self, name, race, char_class, weapon, health_points, mana_points):
         self.name = name
         self.race = race
         self.char_class = char_class
         self.weapon = weapon
-        self.health_points = health_points
-        self.mana_points = mana_points
+        self.health_points = health_points + race.hp_bonus
+        self.mana_points = mana_points + race.mp_bonus
 
 
     def attack(self, target):
@@ -14,4 +16,4 @@ class Character:
         return f"{self.name} attaked {target.name}... {target.name} has {target.health_points} left"
 
     def __str__(self):
-        return f"-------- Character --------\n \tName: {self.name}\n \tRace: {self.race}\n \tClass: {self.char_class}\n \tWeapon: {self.weapon}\n \tHP: {self.health_points}\n \tMP: {self.mana_points}"
+        return f"-------- Character --------\n \tName: {self.name}\n \tRace: {self.race.value}\n \tClass: {self.char_class}\n \tWeapon: {self.weapon}\n \tHP: {self.health_points}\n \tMP: {self.mana_points}"
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from races import Race
 
 def main():
     aragorn = Character("Aragorn", Race.Human, "Ranger", "Sword", 100, 100)
-    borromir = Character("Borromir", Race.Human, "Ranger", "Sword",100, 100)
+    borromir = Character("Borromir", Race.Orc, "Ranger", "Sword",100, 100)
 
     # print(aragorn.attack(borromir))
-    print(aragorn)
+    print(borromir)
 
 if __name__ == "__main__":
     main()
