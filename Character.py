78fc3b94b23752c4f6d20a1db219283395eb4d80
Dragoş <edragos33@gmail.com>
2025-09-03 16:31:44 +0200

    to string added

--- Character.py
+++ Character.py
@@ -7,3 +7,5 @@ class Character:
         self.health_points = health_points
         self.mana_points = mana_points
 
+    def __str__(self):
+        return f"-------- Character --------\n \tName: {self.name}\n \tRace: {self.race}\n \tClass: {self.char_class}\n \tWeapon: {self.weapon}\n \tHP: {self.health_points}\n \tMP: {self.mana_points}"
